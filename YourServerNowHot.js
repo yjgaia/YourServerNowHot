9d0c027d5d52c7176727fc47f834889f02a4ec13
Hanul <hanul@hanul.me>
2019-07-14 13:24:39 +0900

    BOX화

--- YourServerNowHot.js
+++ YourServerNowHot.js
@@ -1,46 +1,31 @@
-require('uppercase-core');
+require(process.env.UPPERCASE_PATH + '/LOAD.js');
 
-BOX('UMAIL');
-
-require('./UMAIL/NODE.js');
-
-let mailConfig = JSON.parse(READ_FILE({
-	path : 'mailconfig.json',
-	isSync : true
-}));
-
-UMAIL.CONNECT_TO_MAIL_SERVER(mailConfig, (_sendMail) => {
+BOOT({
 	
-	let sendMail = (title, content) => {
-		_sendMail({
-			senderName : mailConfig.senderName,
-			senderAddress : mailConfig.senderAddress,
-			receiverAddress : mailConfig.receiverAddress,
-			title : title,
-			content : content + '\n\nBy https://github.com/Hanul/YourServerNowHot'
-		});
-	};
+	CONFIG : {
+		defaultBoxName : 'YourServerNowHot',
+		isDevMode : true
+	},
 	
-	// 1분에 한번씩 CPU와 메모리, 디스크 사용률을 체크
-	INTERVAL(60, RAR(() => {
+	NODE_CONFIG : {
+		// 테스트 목적이기 때문에 CPU 클러스터링 기능을 사용하지 않습니다.
+		isSingleCoreMode : true,
 		
-		EACH(CPU_USAGES(), (cpuUsage) => {
-			if (cpuUsage > 90) {
-				sendMail(mailConfig.serverName + '의 CPU 사용률이 ' + cpuUsage + '%에 육박하였습니다.', 'CPU 사용률이 ' + cpuUsage + '%에 육박하였습니다.\n' + mailConfig.serverName + '을(를) 체크하시기 바랍니다.');
+		YourServerNowHot : {
+			
+			serverName : 'My Server',
+			
+			mail : {
+				host : 'smtp.gmail.com',
+				port : 465,
+				isSecure : true,
+				username : 'sender@hanul.me',
+				password : 'ENTER YOUR PASSWORD',
+				
+				senderName : 'BTNcafe Contact',
+				senderAddress : 'sender@hanul.me',
+				receiverAddress : 'hanul@hanul.me'
 			}
-		});
-		
-		let memoryUsage = MEMORY_USAGE();
-		if (memoryUsage > 90) {
-			sendMail(mailConfig.serverName + '의 메모리 사용률이 ' + memoryUsage + '%에 육박하였습니다.', '메모리 사용률이 ' + memoryUsage + '%에 육박하였습니다.\n' + mailConfig.serverName + '을(를) 체크하시기 바랍니다.');
 		}
-		
-		DISK_USAGE((diskUsage) => {
-			if (diskUsage > 90) {
-				sendMail(mailConfig.serverName + '의 디스크 사용률이 ' + diskUsage + '%에 육박하였습니다.', '디스크 사용률이 ' + diskUsage + '%에 육박하였습니다.\n' + mailConfig.serverName + '을(를) 체크하시기 바랍니다.');
-			}
-		});
-	}));
-	
-	console.log(CONSOLE_GREEN('Your Server Now Hot이 실행중입니다...'));
+	}
 });
---
+++ YourServerNowHot/NODE/Deamon.js
@@ -0,0 +1,49 @@
+YourServerNowHot.Deamon = OBJECT({
+	
+	init : () => {
+		
+		DISTRIBUTE_PROCESS({
+			tag : 'Your Server Now Hot'
+		}, () => {
+			
+			let serverName = NODE_CONFIG.YourServerNowHot.serverName;
+			let mailConfig = NODE_CONFIG.YourServerNowHot.mail;
+			
+			UMAIL.CONNECT_TO_MAIL_SERVER(mailConfig, (_sendMail) => {
+				
+				let sendMail = (title, content) => {
+					_sendMail({
+						senderName : mailConfig.senderName,
+						senderAddress : mailConfig.senderAddress,
+						receiverAddress : mailConfig.receiverAddress,
+						title : title,
+						content : content + '\n\nBy https://github.com/Hanul/YourServerNowHot'
+					});
+				};
+				
+				// 1분에 한번씩 CPU와 메모리, 디스크 사용률을 체크
+				INTERVAL(60, RAR(() => {
+					
+					EACH(CPU_USAGES(), (cpuUsage) => {
+						if (cpuUsage > 90) {
+							sendMail(serverName + '의 CPU 사용률이 ' + cpuUsage + '%에 육박하였습니다.', 'CPU 사용률이 ' + cpuUsage + '%에 육박하였습니다.\n' + serverName + '을(를) 체크하시기 바랍니다.');
+						}
+					});
+					
+					let memoryUsage = MEMORY_USAGE();
+					if (memoryUsage > 90) {
+						sendMail(serverName + '의 메모리 사용률이 ' + memoryUsage + '%에 육박하였습니다.', '메모리 사용률이 ' + memoryUsage + '%에 육박하였습니다.\n' + serverName + '을(를) 체크하시기 바랍니다.');
+					}
+					
+					DISK_USAGE((diskUsage) => {
+						if (diskUsage > 90) {
+							sendMail(serverName + '의 디스크 사용률이 ' + diskUsage + '%에 육박하였습니다.', '디스크 사용률이 ' + diskUsage + '%에 육박하였습니다.\n' + serverName + '을(를) 체크하시기 바랍니다.');
+						}
+					});
+				}));
+				
+				console.log(CONSOLE_GREEN('Your Server Now Hot이 실행중입니다...'));
+			});
+		});
+	}
+});
