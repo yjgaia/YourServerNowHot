6faf1fc348d50d22df6ce1d830e4b24a064f0603
hanul <hanul@hanul.me>
2020-12-31 09:45:24 +0900

    jobs 설정 개발중

--- YourServerNowHot.js
+++ YourServerNowHot.js
@@ -29,6 +29,19 @@ BOOT({
 			
 			// checkURL 설정을 하게되면 이 URL에 요청을 보내 오류가 반환되면 메일을 보내줍니다.
 			// checkURL : 'http://localhost:8888/check'
+
+			// jobs 설정을 통해 서버에 무리가 발생할 때 실행할 명령들을 작성합니다.
+			/* jobs : [{
+				type : 'request',
+				url : 'http://other-server.com:8080/server-now-hot'
+			}, {
+				type : 'command',
+				delay : 10, // 10초 뒤 실행
+				folderPath : '/home/dev',
+				commands : [
+					'forever restartall'
+				]
+			}] */
 		}
 	}
 });
